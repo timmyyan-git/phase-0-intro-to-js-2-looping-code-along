b308f98fcc14bcb7fefdf1d1d84279cf4bc03e80
Timmy Yan <timmyyan2000@gmail.com>
2022-11-01 11:46:08 -0400

    commit tree 3769a02c4ccc5bbb63ad96501ca308e73157d1df

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+    
+    const newArr = []
+    
+    for (let i = 0; i < names.length; i++) {
+        const customMsg = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+        console.log(customMsg)
+        newArr.push(customMsg) 
+    }
+    
+    return newArr 
+}
+
+function countDown() {
+    let countDown = 11;
+    while(countDown > 0){
+        countDown--;
+        console.log(countDown);
+    }
+}
